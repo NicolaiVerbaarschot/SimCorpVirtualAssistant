7c7512c5daad34100a530aeef518235af31ad1f5
Jakob Offersen <jakoboffersen@gmail.com>
2019-03-22 16:30:55 +0100

    Moved speech into own file

--- myapp/public/javascripts/scripts.js
+++ myapp/public/javascripts/scripts.js
@@ -1,45 +1,6 @@
 var accessToken = "fe3ac7ce30b340d1b6802eb18de04809";
 var baseUrl = "https://api.api.ai/v1/";
 
-var recognition;
-
-function startRecognition() {
-    recognition = new webkitSpeechRecognition();
-    recognition.onstart = function(event) {
-        updateRec();
-    };
-    recognition.onresult = function(event) {
-        var text = "";
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            text += event.results[i][0].transcript;
-        }
-        setInput(text);
-        network.send(text);
-        stopRecognition();
-    };
-    recognition.onend = function() {
-        stopRecognition();
-    };
-    recognition.lang = "en-US";
-    recognition.start();
-}
-
-function stopRecognition() {
-    if (recognition) {
-        recognition.stop();
-        recognition = null;
-    }
-    updateRec();
-}
-
-function switchRecognition() {
-    if (recognition) {
-        stopRecognition();
-    } else {
-        startRecognition();
-    }
-}
-
 function setInput(text) {
     $("#input").val(text);
 }
