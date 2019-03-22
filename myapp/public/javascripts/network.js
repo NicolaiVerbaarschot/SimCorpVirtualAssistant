f76e2990ec841a46e02a2a98eefb3500dd13009c
Jakob Offersen <jakoboffersen@gmail.com>
2019-03-22 16:22:17 +0100

    Refactored send function into prototype

--- myapp/public/javascripts/network.js
+++ myapp/public/javascripts/network.js
@@ -12,20 +12,21 @@ var network = new Network(successHandler, errorHandler);
 function Network(successHandler, errorHandler) {
     this.successHandler = successHandler; 
     this.errorHandler = errorHandler;
-    this.send = function(value) {
-        setResponse("You: " + value);
-        $.ajax({
-            type: "POST",
-            url: baseUrl + "query?v=20150910",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            headers: {
-                "Authorization": "Bearer " + accessToken
-            },
-            data: JSON.stringify({ query: value, lang: "en", sessionId: "somerandomthing" }),
-    
-            success: successHandler,
-            error: errorHandler
-        });
-    }
+}
+
+Network.prototype.send = function(value) {
+    setResponse("You: " + value);
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "query?v=20150910",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + accessToken
+        },
+        data: JSON.stringify({ query: value, lang: "en", sessionId: "somerandomthing" }),
+
+        success: successHandler,
+        error: errorHandler
+    });
 }
