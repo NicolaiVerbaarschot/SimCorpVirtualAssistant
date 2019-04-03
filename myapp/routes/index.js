560a294980b02d9af46b16bdef703bf20a6c3f39
Simon El Nahas Christensen <s163951@student.dtu.dk>
2019-04-03 17:06:19 +0200

    WIP implementing action-on-google Fulfillment

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,52 +1,41 @@
-const express = require('express');
-const router = express.Router();
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const functions = require('firebase-functions')
+const { dialogflow } = require('actions-on-google')
 
-/* GET home page. */
+const app = dialogflow()
 
-var mysql = require('mysql');
+app.intent('Default Welcome Intent', conv => {
+  conv.ask('Welcome to my agent!')
+  console.log("welcome was asked")
+})
 
-var con = mysql.createConnection({
-  host: "remotemysql.com",
-  user: "yiZaQZM5Nm",
-  password: "L3YF0CxQf7",
-  database: "yiZaQZM5Nm"
-});
+app.intent('Default Fallback Intent', conv => {
+  conv.ask(`I didn't understand`)
+  conv.ask(`I'm sorry, can you try again?`)
+})
 
+app.intent('name', conv => {
+  conv.ask('My name IS DialogfloWWW!')
+  console.log("name was asked")
+})
 
-function makeConnectionToDB() {
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to Database!");
-  });
-}
-makeConnectionToDB();
+app.intent('Knowledge.KnowledgeBase.NjUxNTM1NDA2MjQ3OTY4NzY4MA', conv => {
+  //conv.ask('Knowledge database stuff');
+  console.log("asked KB");
+})
 
-function queryDB(res,query) {
-  con.query(query, function (err, data) {
-    if (err) throw err;
-    console.log("made query: "+query);
-    res.render('table.ejs', {results: data});
-  });
-}
-
-router.get('/api/:query', function(req, res) {
-  queryDB(res,req.params.query);
-});
-
-router.get('/table',function (req,res) {
-  res.render('table.html');
-
-});
-
-router.get('/', function(req, res, next) {
-  res.render('client.html' , {result_from_database: ""});
-
-});
-
-router.get('/graph', function(req, res, next) {
-  res.render('graph.html');
-
-});
-
-module.exports = router;
-
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
---
+++ myapp/routes/index2.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "remotemysql.com",
+    user: "yiZaQZM5Nm",
+    password: "L3YF0CxQf7",
+    database: "yiZaQZM5Nm"
+});
+
+
+function makeConnectionToDB() {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected to Database!");
+    });
+}
+makeConnectionToDB();
+
+function queryDB(res,query) {
+    con.query(query, function (err, data) {
+        if (err) throw err;
+        console.log("made query: "+query);
+        res.render('table.ejs', {results: data});
+    });
+}
+
+router.get('/api/:query', function(req, res) {
+    queryDB(res,req.params.query);
+});
+
+router.get('/table',function (req,res) {
+    res.render('table.html');
+
+});
+
+router.get('/', function(req, res, next) {
+    res.render('client.html' , {result_from_database: ""});
+
+});
+
+router.get('/graph', function(req, res, next) {
+    res.render('graph.html');
+
+});
+
+module.exports = router;
+
