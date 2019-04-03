0ea291a70c4bb4c20048eb6d672ea673ee81dc0d
NicolaiV <nicolai.verbaarschot@gmail.com>
2019-04-03 19:55:15 +0200

    Removal of unused code and files. Also made it so that pressing enter while focused on the query form, it clicks the fetch database button. *Small clean up

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
-var cors = require('cors')
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(cors())
+app.use(cors());
 
 // view engine setup to make it possible to render plain html
 app.set('views', path.join(__dirname, 'views'));
@@ -13,10 +13,10 @@ app.engine('html', require('ejs').renderFile);
 
 //setup dir to search through when looking for files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public/javascripts')))
+app.use(express.static(path.join(__dirname, 'public/javascripts')));
 
 app.use('/', indexRouter);
 
-app.listen(3000, function() {console.log(`listening at the moment!`)})
+app.listen(3000, function() {console.log(`listening at the moment!`)});
 
 module.exports = app;
