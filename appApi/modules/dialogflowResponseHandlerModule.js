62440da093e77d0596de3fc983cffb2eba6ba60b
NicolaiV <nicolai.verbaarschot@gmail.com>
2019-04-25 00:01:15 +0200

    Refactored async calls further. Now using mapping for rendering ejs files which makes it much more expandable.

--- appApi/modules/dialogflowResponseHandlerModule.js
+++ appApi/modules/dialogflowResponseHandlerModule.js
@@ -1,16 +1,21 @@
 const render = require('consolidate');
 const path = require('path');
 
-
-var resolvedResponseData = {
-    actionType: undefined,
-    parameters: undefined,
-    answer: undefined,
-    newTable: undefined,
-    newVisualisation: undefined
+const responseFieldMap = {
+    textOP: '',
+    tableOP: 'newTable',
+    graphOP: 'newGraph'
 };
 
+const templateMap = {
+    textOP: 'testejs',
+    tableOP: 'testejs',
+    graphOP: 'graphTemplate'
+};
+
+
 async function renderEjs(templateName) {
+
     let htmlOuter;
     await render.ejs(path.resolve(__dirname,'../ejsTemplates/'+templateName+'.ejs'), { results: ['test1','test2'] }) // TODO: implement parameter handling
         .then(function (html) {
@@ -24,36 +29,31 @@ async function renderEjs(templateName) {
 
 async function handleDialogflowResponse(response) {
 
+    // Declare return object defaults
+    let resolvedResponseData = {
+        actionType: undefined,
+        parameters: undefined,
+        answer: undefined,
+        newTable: undefined,
+        newVisualisation: undefined
+    };
+
     // Action type is resolved from intent name by splitting on underscore character
-    resolvedResponseData.actionType = response.intentName.substring(0, response.intentName.indexOf('_'));
+    const actionType = response.intentName.substring(0, response.intentName.indexOf('_'));
+
+    // Render ejs templates according to action type
+    if (['tableOP','graphOP'].includes(actionType))
+        await renderEjs(templateMap[actionType]).then((html) => {
+            resolvedResponseData[responseFieldMap[actionType]] = html;
+        });
+
+    // Define remaining properties
+    resolvedResponseData.actionType = actionType;
     resolvedResponseData.parameters = response.parameters;
     resolvedResponseData.answer = response.answer;
 
-    // Render ejs templates in case of applicable action type
-    switch (resolvedResponseData.actionType) {
-
-        case 'textOP':
-            await renderEjs('testejs').then((html) => {
-                resolvedResponseData.newTable = html;
-            }); break;
-
-        case 'tableOP':
-            await renderEjs('testejs').then((html) => {
-                resolvedResponseData.newTable = html;
-            }); break;
-
-        case 'graphOP':
-            await renderEjs('testejs').then((html) => {
-                resolvedResponseData.newTable = html;
-            }); break;
-        default:
-            return resolvedResponseData;
-    }
-
     return resolvedResponseData;
 }
-
-
 
 
 module.exports.resolve = handleDialogflowResponse;
