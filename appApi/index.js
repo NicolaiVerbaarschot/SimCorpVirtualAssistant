dedaf47d960905d7acbd1be10a2b660d6d44fcb3
SimonElNahasChristensen <s163951@student.dtu.dk>
2019-04-24 11:01:09 +0200

    WIP http request on backend

--- appApi/index.js
+++ appApi/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const rest = require(path.resolve(__dirname, "./modules/httpRequest")); //TODO make more nice nicolai
 const database = require(path.resolve(__dirname, "./modules/databaseModule"));
 const documentSearch = require(path.resolve(__dirname, "./modules/documentSearch"));
 const superuserCommandHandler = require(path.resolve(__dirname, "./modules/superuserModule"));
@@ -15,10 +16,97 @@ router.get('/api/superuser/:query', function(req, res) {
 
 });
 
+
+const accessToken = "fe3ac7ce30b340d1b6802eb18de04809";
+const baseUrl = "https://api.api.ai/v1/";
+const localHost = "http://localhost:8080/";
+
 router.get('/api/chatBotQueryManager/:query', function (req,res) {
+  let message = req.params.query;
+  console.log(message);
+  //res.send("answering you")
+
+  const options = {
+    host: "https://google.com",//baseUrl+"query?v=20150910",
+    port: 443,
+    path: '',
+    method: 'GET',
+    headers: {
+      'Authorization': "Bearer " + accessToken
+    }
+  };
+  rest.getJSON(options,(statusCode,result) => {
+    console.log(`onResult: (${statusCode})\n\n${JSON.stringify(result)}`);
+
+    res.statusCode = statusCode;
+
+    res.send(result);
+
+  })
 
 
-})
+  //
+  //
+  // let output = '';
+  //
+  // const req1 = http.request(options, (res) => {
+  //   console.log(`${options.host} : ${res.statusCode}`);
+  //   res.setEncoding('utf8');
+  //
+  //   res.on('data', (chunk) => {
+  //     output += chunk;
+  //   });
+  //
+  //   res.on('end', () => {
+  //     let obj = JSON.parse(output);
+  //     console.log("ended: ");
+  //     console.log(obj);
+  //
+  //     //onResult(res.statusCode, obj);
+  //   });
+  //
+  //
+  //
+  // });
+  //
+  //
+  //
+  // req1.on('error', (err) => {
+  //   // res.send('error: ' + err.message);
+  // });
+  //
+  // req1.end();
+ // sendAsync(message).then((data) => {
+ //   console.log(data);
+ //   res.send(data)
+//
+//
+ // }
+ // );
+
+});
+
+const sendAsync = async function(value) {
+  let result;
+  try {
+    result = await jquery.$.ajax({
+      type: "POST",
+      url: baseUrl + "query?v=20150910",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      headers: {
+        "Authorization": "Bearer " + accessToken
+      },
+      data: JSON.stringify({ query: value, lang: "en", sessionId: "somerandomthing" }),
+    });
+
+    return result;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 router.get('/api/search/:query', function(req, res) {
   const fuse = documentSearch.fuse;
