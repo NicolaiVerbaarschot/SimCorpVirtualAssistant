039b18866811b1e156c85e1c05c4ac0d4b93de6a
SimonElNahasChristensen <s163951@student.dtu.dk>
2019-05-08 10:53:45 +0200

    returns all parameters as before, now also working with KB

--- appApi/modules/dialogflowInterfaceModule.js
+++ appApi/modules/dialogflowInterfaceModule.js
@@ -1,4 +1,6 @@
 const najax = require('najax');
+const dialogflow = require('dialogflow').v2beta1;
+const uuid = require('uuid');
 
 const accessToken = "fe3ac7ce30b340d1b6802eb18de04809";
 const baseUrl = "https://api.api.ai/v1/";
@@ -8,21 +10,20 @@ var config = {
     projectId: 'firstbot-d1b5b',
     keyFilename: 'appApi/firstbot-d1b5b-e44bae98475c.json',
 };
-const dialogflow = require('dialogflow');
-const uuid = require('uuid');
+const projectId = 'firstbot-d1b5b';
+// A unique identifier for the given session
+const sessionId = uuid.v4();
+const languageCode = 'en';
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient(config);
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 /**
  * Send a query to the dialogflow agent, and return the query result.
  * @param {string} projectId The project to be used
  */
-async function postToDialogflow(value) {
-    const projectId = 'firstbot-d1b5b';
-    // A unique identifier for the given session
-    const sessionId = uuid.v4();
-
-    // Create a new session
-    const sessionClient = new dialogflow.SessionsClient(config);
-    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+async function postToDialogflow(query) {
 
     // The text query request.
     const request = {
@@ -30,34 +31,38 @@ async function postToDialogflow(value) {
         queryInput: {
             text: {
                 // The query to send to the dialogflow agent
-                text: value,
+                text: query,
                 // The language used by the client (en-US)
-                languageCode: 'en',
+                languageCode: languageCode,
             },
         },
     };
 
     // Send request and log result
     const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
-        let isKnowledgeAnswer = !result.alternativeQueryResults;
-        let allRequiredParamsPresent = result.allRequiredParamsPresent;
 
-        return {
-            answer: result.fulfillmentText,
-            action: isKnowledgeAnswer ? "Knowledge" : result.action,
-            allRequiredParamsPresent : allRequiredParamsPresent,
-            parameters: allRequiredParamsPresent ? result.parameters : undefined,
-            intentName: result.intent.displayName
-        };
+
+    console.log('Detected intent');
+    const queryResult = responses[0].queryResult;
+    console.log(`  Query: ${queryResult.queryText}`);
+    console.log(`  Response: ${queryResult.fulfillmentText}`);
+    let isKnowledgeAnswer = queryResult.action==="";
+    let allRequiredParamsPresent = queryResult.allRequiredParamsPresent;
+
+    if (queryResult.intent) {
+        console.log(`  Intent: ${queryResult.intent.displayName}`);
+
     } else {
         console.log(`  No intent matched.`);
     }
+    return {
+        answer: queryResult.fulfillmentText,
+        action: isKnowledgeAnswer ? "Knowledge" : queryResult.action,
+        allRequiredParamsPresent : allRequiredParamsPresent,
+        parameters: queryResult.allRequiredParamsPresent ? queryResult.parameters : undefined,
+        intentName: queryResult.intent.displayName
+    };
+
 }
 
 async function postToDialogflowOld(value) {
