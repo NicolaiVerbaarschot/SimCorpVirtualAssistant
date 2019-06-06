fd5010c34ab67042a93680ae76978939f3ab4072
NicolaiV <nicolai.verbaarschot@gmail.com>
2019-06-06 14:55:30 +0200

    Fixed search div.

--- appWeb/javascripts/botQueryController.js
+++ appWeb/javascripts/botQueryController.js
@@ -6,20 +6,18 @@ const bot_DOM_QueryController = {
         setResponse("Bot: " + data.answer);
 
         console.log(data.actionType);
+
         if (data.actionType) {
 
             switch (data.actionType) {
                 case 'tableOP':
                     $("#databaseContainer").html(data.newTable.toString());
                     break;
-                // case 'Knowledge':
-                //     $("#fuseContainer").html(data.knowledgeAnswer.toString());
-                //     break;
                 case 'graphOP':
                     $("#graphContainer").html(data.newGraph.toString());
                     break;
-
             }
+
         } else {
             $("#fuseContainer").html(data.answer.toString());
         }
